c1bc8452da462aeed4765df83705d2350d808e6f
Men Pisey <kunhseybro@gmail.com>
2024-07-18 21:28:13 +0700

    Connection Database Entity User Role

---
+++ src/main/java/com/menpisey/auth_awt/model/user/Role.java
@@ -0,0 +1,10 @@
+package com.menpisey.auth_awt.model.user;
+
+public enum Role {
+    ROLE_ADMIN,
+    ROLE_USER,
+    ROLE_MODERATOR,
+    ROLE_MANAGER,
+    ROLE_EMPLOYEE
+
+}
---
+++ src/test/java/com/menpisey/auth_awt/AuthAwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.menpisey.auth_awt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthAwtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
